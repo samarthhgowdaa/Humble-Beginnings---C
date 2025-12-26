c0344bbd49b2256c307fa3ffee64688aa2756a0a
Samarth Gowda <samarthac4work@gmail.com>
2025-12-26 21:05:44 +0530

    Completed learning about math functions

--- 05-mathFunctions/main.c
+++ 05-mathFunctions/main.c
@@ -3,8 +3,26 @@
 
 int main()
 {
+    float x = 3;
+    float y = 3.1549;
+    float z = 3;
+    float a = 45;
+
+    //x = sqrt(x); //without that header file, this won't work
+    x = pow(x, 3); // x cube or x power 3
+    //y = round(y);  //rounds the number
+    y = ceil(y); //rounds up
+    //y = floor(y); //rounds down
+    //z = abs(z); //Absolute value of z
+    z = log(z);
+    a = sin(a); //will be in radians, same goes for cos, tan, etc
 
 
+
+    printf("%f \n", x);
+    printf("%f \n", y);
+    printf("%f \n", z);
+    printf("%f \n", a);
 
     return 0;
 }
