5b859eda9c921a5ec000371267d8d3ed1ee85865
Samarth Gowda <samarthac4work@gmail.com>
2026-02-17 20:20:49 +0530

    Completed learning about if statements

--- 06-ifStatements/main.c
+++ 06-ifStatements/main.c
@@ -1,2 +1,23 @@
 #include<stdio.h>
 #include<math.h>
+
+int main (){
+
+    int age;
+
+    printf("Enter your age: ");
+    scanf("%d", &age);
+
+    if(age >= 18){
+        printf("You are an adult");
+    }
+    else if (age > 18)
+    {
+        printf("you are are an adult");
+    }
+    else{
+        printf("you are dead!");
+    }
+    
+    return 0;
+}
